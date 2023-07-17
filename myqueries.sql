b9871c57ea2555e7e6b4bb2de71d15a3dfbe0780
kaminenirakesh <kaminenirakesh001@gmail.com>
2023-07-17 06:27:10 +0000

    2nd query on sales table

--- myqueries.sql
+++ myqueries.sql
@@ -1,8 +1,11 @@
--- description of sales table
+-- description of sales table.
 desc sales;
 
--- fetch all records from the sales table
+-- fetch all records from the sales table.
 select * from sales; 
 
--- query to find total customer list who are all ordered from the USA
+-- query to find total customer list who are all ordered from the USA.
 select count(*) as total_customer_list from sales where country ='USA';
+
+-- query to find no of customers who have made purchases exceeding 5000.
+select count(*) as no_of_customers from sales where sales>5000;
