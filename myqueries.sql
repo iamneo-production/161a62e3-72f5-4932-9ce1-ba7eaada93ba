25c360f0589cd1910106130e2d7f1e4d95918b76
kaminenirakesh <kaminenirakesh001@gmail.com>
2023-07-18 09:00:40 +0000

    5th query on sales table

--- myqueries.sql
+++ myqueries.sql
@@ -15,3 +15,6 @@ select count(*) as no_of_products from sales where year_id = '2003';
 
 -- query to find total sale price in year 2005.
 select sum(sales) as total_sale_price from sales where year_id = '2005';
+
+-- query to find total sale price for each year.
+select sum(sales) as total_sale_price from sales group by year_id;
