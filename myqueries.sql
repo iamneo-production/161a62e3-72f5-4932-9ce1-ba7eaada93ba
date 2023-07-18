bb57cabc8360b0d162902464984247b4153a1a86
kaminenirakesh <kaminenirakesh001@gmail.com>
2023-07-18 08:56:17 +0000

    4th query

--- myqueries.sql
+++ myqueries.sql
@@ -12,3 +12,6 @@ select count(*) as no_of_customers from sales where sales>5000;
 
 -- query to find no of products ordered in 2003
 select count(*) as no_of_products from sales where year_id = '2003';
+
+-- query to find total sale price in year 2005.
+select sum(sales) as total_sale_price from sales where year_id = '2005';
