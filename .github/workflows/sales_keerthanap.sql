da8dbcef1192fd25c7a47172923735221b0f1d25
Keerthana-123-keerthi <keerthikeerthana1308@gmail.com>
2023-07-23 07:17:17 +0000

    commited

--- .github/workflows/sales_keerthanap.sql
+++ .github/workflows/sales_keerthanap.sql
@@ -1,21 +1,19 @@
-select count(*) as customer_list
-from SALES
+--query to find total customer list who are all ordered from USA.
+select count(*) as customer_list from sales
 where country='USA';
 
-select count(*) as no_of_customer
-from SALES
-where priceeach>5000;
+--query to find no of customer who have made purchases exceeding 5000.
+select count(*) as no_of_customer from sales
+where sales>5000;
 
-select count(*) as no_of_products
-from SALES
+--query to find no of products ordered in 2003.
+select count(*) as no_of_products from sales
 where year_id=2003;
 
-select sum(sales) as tot_sale_price
-from sales
+--query to find total sale price in year 2005. 
+select sum(sales) as tot_sale_price from sales
 where year_id=2005;
 
-
-select year_id,sum(sales) as tot_sale_price
-from sales
-group by year_id
-order by year_id;
+--query to find total sale price for each year.
+select year_id,sum(sales) as tot_sale_price from sales
+group by year_id;
