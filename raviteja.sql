860462fc29aea1f13ea70557f71a86adae97f84e
Ravitejakammula-11 <ravitejakammula2001@gmail.com>
2023-07-30 08:25:38 +0000

    query commited

--- raviteja.sql
+++ raviteja.sql
@@ -6,10 +6,10 @@ desc sales;
 select * from sales;
 
 --query to find total customer list who are all  ordered from the USA
-select count(*) as customer_list from sales where country='USA';
+select count(*) as Customer_list from sales where country='USA';
 
 --query to find no of customers who have made purchases exceeding 5000
-select count(*) as purchase_list from sales where sales>5000;
+select count(*) as Purchase_list from sales where sales>5000;
 
 --query to find no of products ordered in 2003
 select count(ordernumber) as product_list from sales where year_id=2003;
