3cf76de1c0adc6bac8d0ef73815b4bb0fb012e38
Ravitejakammula-11 <ravitejakammula2001@gmail.com>
2023-07-30 08:14:19 +0000

    query commited

--- raviteja.sql
+++ raviteja.sql
@@ -6,29 +6,18 @@ desc sales;
 select * from sales;
 
 --query to find total customer list who are all  ordered from the USA
-select count(*) as customers_list
-from sales
-where country='USA';
+select count(*) as customer_list from sales where country='USA';
 
 --query to find no of customers who have made purchases exceeding 5000
-select count(*) as purchases_list
-from sales
-where sales>5000;
+select count(*) as purchase_list from sales where sales>5000;
 
 --query to find no of products ordered in 2003
-select count(ordernumber) as product_list
-from sales
-where year_id=2003;
+select count(ordernumber) as product_list from sales where year_id=2003;
 
 --query to find total sales price in year 2005
-select sum(sales) as total_sales
-from sales
-where year_id=2005;
+select sum(sales) as total_sales from sales where year_id=2005;
 
 --query to find total sale price for each year
 
-select year_id,sum(sales) as total_sale_Price
-from sales
-group by year_id
-order by year_id;
+select year_id,sum(sales) as total_sale_Price from sales group by year_id order by year_id;
 
