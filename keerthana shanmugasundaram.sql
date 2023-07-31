d3ff7cc17149fcb9da348001983d9cdc12180fa4
keerthikeerthana39 <keerthana.sbtechit@gmail.com>
2023-07-31 08:15:06 +0000

    commit

--- keerthana shanmugasundaram.sql
+++ keerthana shanmugasundaram.sql
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-=======
-<<<<<<<< HEAD:sales_keerthanas.sql
---description of sales_history 
-desc sales;
-========
->>>>>>>> Oracle-Workspace-keerthikeerthana39:keerthana shanmugasundaram.sql
->>>>>>> Oracle-Workspace-keerthikeerthana39
 
 --to find total_customer list who are all ordered from the USA
 select count(*) as total_customer_list from sales where country = 'USA';
@@ -22,9 +14,3 @@ select sum(sales) as total_sale_price from sales where year_id= '2005';
 --to find total_sale_price for each year
 select sum(sales) as total_sale_price from sales group by year_id;
 
-<<<<<<< HEAD
-=======
-
-
-
->>>>>>> Oracle-Workspace-keerthikeerthana39
